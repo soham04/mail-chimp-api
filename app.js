8df77f6420b3ffe0e549b0de1f088a12340b64c2
Soham Shinde <sohamshinde04@gmail.com>
2021-08-01 12:38:24 +0530

    fixed the API key

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
+//Requiring mailchimp's module
+//For this we need to install the npm module @mailchimp/mailchimp_marketing. To do that we write:
+//npm install @mailchimp/mailchimp_marketing
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+//Requiring express and body parser and initializing the constant "app"
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-const port = process.env.PORT || 3000;
 const app = express();
-const https = require("https");
-const client = require("@mailchimp/mailchimp_marketing");
+//Using body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+//The public folder which holds the CSS
+app.use(express.static("public"));
+//Listening on port 3000 and if it goes well then logging a message saying that the server is running
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running at port 3000");
+});
+//Sending the signup.html file to the browser as soon as a request is made on localhost:3000
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+  console.log("send signup html");
+});
 
-client.setConfig({
-  apiKey: "7412d42a898c8ee31516ccd2b5504ec9-us6",
+//Setting up MailChimp
+mailchimp.setConfig({
+  //*****************************ENTER YOUR API KEY HERE******************************
+  apiKey: "db8600e2ebf0fcc4ce2bc3ce0b38f80b-us6",
+  //*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
   server: "us6",
 });
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/signup.html");
-  console.log("got a request");
-});
-
+//As soon as the sign in button is pressed execute this
 app.post("/", function (req, res) {
-  console.log(req.body.First_Name);
-  console.log(req.body.Second_Name);
-  const fname = req.body.First_Name;
-  const lname = req.body.Second_Name;
+  //*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
+  const firstName = req.body.First_Name;
+  const secondName = req.body.Second_Name;
   const email = req.body.email;
-
-  let subscribingUser = {
-    firstName: fname,
-    lastName: lname,
+  //*****************************ENTER YOU LIST ID HERE******************************
+  const listId = "4e368f6f33";
+  //Creating an object with the users data
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: secondName,
     email: email,
   };
-
+  //Uploading the data to the server
+  // async function run() {
   const run = async () => {
-    const response = await client.lists.addListMember("4e368f6f33", {
+    const response = await mailchimp.lists.addListMember(listId, {
       email_address: subscribingUser.email,
       status: "subscribed",
       merge_fields: {
@@ -41,19 +52,14 @@ app.post("/", function (req, res) {
         LNAME: subscribingUser.lastName,
       },
     });
-    console.log(response); // (optional)
+    //If all goes well logging the contact's id
+    res.sendFile(__dirname + "/success.html");
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
   };
-  console.log("hi");
-
-  run();
+  //Running the function and catching the errors (if any)
+  // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
+  // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+  run().catch((e) => res.sendFile(__dirname + "/failure.html"));
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// API key
-// 7412d42a898c8ee31516ccd2b5504ec9-us6
-
-// List ID
-// 4e368f6f33
